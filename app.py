6eb3d7bb95d4faad8d099f60a8a372fa5a31a941
ivin jose <ivinnjose@gmail.com>
2025-05-04 22:19:13 +0530

    update app name5

--- app.py
+++ app.py
@@ -131,7 +131,8 @@ def add_data():
 
         except Exception as e:
             mysql.connection.rollback()
-            return redirect('/home')
+    		return render_template('index.html', message=e)
+            
 
 
 
